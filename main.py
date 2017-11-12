8ebe1080f4bb72dc8bdbcc2b6bc02feea00e024e
aferral <andreslago5@hotmail.com>
2017-11-12 13:58:44 -0300

    Va bien el filtrado ya separe el texto de la nada. Lo unico malo que queda entremedio de manchas

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+from skimage.io import imread
+from skimage.exposure import histogram,equalize_adapthist,equalize_hist
+from skimage.filters import threshold_otsu,threshold_yen
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 # Leer imagen de apuntes
+ruta = "imagenes/evaluacionP/evalproj-04.png"
 
+im = imread(ruta,as_grey=True)
+
+
+bin1 = im > threshold_otsu(im)
+bin2 = im > threshold_yen(im)
+
+eq1=equalize_hist(im)
+eq2=equalize_adapthist(im)
+
+
+f,ax=plt.subplots(1,3)
+ax[0].imshow(bin1)
+ax[1].imshow(bin2)
+ax[2].imshow(im)
+plt.show()
+
+print("ho")
 
 # Paso quitar lo que no sea texto o dibujos
 
